83674c39e4cb7a97a83274d0a6408b0b6715f491
evangelinejohanna <evajo812@gmail.com>
2024-12-02 20:05:33 +0530

    Add files via upload

---
+++ EX 6A/connect.php
@@ -0,0 +1,9 @@
+<?php
+// Database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "note_taking_app";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
